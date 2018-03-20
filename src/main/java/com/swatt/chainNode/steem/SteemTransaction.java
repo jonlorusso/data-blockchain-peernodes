35b941f9e82416636cf7410261eecda05fd17171
Unknown <christian@glover-wilson.com>
2018-03-20 13:53:07 -0400

    Steem stub

---
+++ src/main/java/com/swatt/chainNode/steem/SteemTransaction.java
@@ -0,0 +1,43 @@
+package com.swatt.chainNode.steem;
+
+import java.util.logging.Logger;
+
+import com.googlecode.jsonrpc4j.JsonRpcHttpClient;
+import com.swatt.chainNode.ChainNodeTransaction;
+import com.swatt.util.OperationFailedException;
+
+public class SteemTransaction extends ChainNodeTransaction {
+    private static final Logger LOGGER = Logger.getLogger(SteemTransaction.class.getName());
+    private double inValue;
+    private boolean minted = false;
+    private long size;
+
+    SteemTransaction(JsonRpcHttpClient jsonrpcClient, String hash, boolean calculateFee)
+            throws OperationFailedException {
+        super(hash);
+
+        /*
+         * RPCTransaction rpcTransaction = fetchFromBlockchain(jsonrpcClient, hash);
+         * 
+         * this.vout = rpcTransaction.vout;
+         * 
+         * // Compute amount double amount = 0.0;
+         * 
+         * // TODO clean up vout array once values read, no further need for (int i = 0;
+         * i < rpcTransaction.vout.size(); i++) { RPCVout outTransaction =
+         * rpcTransaction.vout.get(i); amount += outTransaction.value; }
+         * 
+         * setAmount(amount); setTimestamp(rpcTransaction.time);
+         * 
+         * // Compute Size
+         * 
+         * if (rpcTransaction.vsize != null) { setSize(rpcTransaction.vsize); } else {
+         * setSize(rpcTransaction.size); }
+         * 
+         * super.setBlockHash(rpcTransaction.blockhash);
+         * 
+         * if (calculateFee) calculateFee(jsonrpcClient, rpcTransaction);
+         */
+    }
+
+}
--- src/main/java/com/swatt/chainNode/xmr/MoneroChainNode.java
+++ src/main/java/com/swatt/chainNode/xmr/MoneroChainNode.java
@@ -1,28 +1,54 @@
 package com.swatt.chainNode.xmr;
 
+import java.math.BigDecimal;
+//For creating URLs
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Instant;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.json.simple.JSONObject;
+
 import com.googlecode.jsonrpc4j.JsonRpcHttpClient;
 import com.swatt.chainNode.ChainNode;
 import com.swatt.chainNode.ChainNodeTransaction;
-import com.swatt.chainNode.btc.BitcoinChainNode;
-import com.swatt.chainNode.btc.RPCBlock;
 import com.swatt.chainNode.dao.BlockData;
 import com.swatt.util.JsonRpcHttpClientPool;
 import com.swatt.util.OperationFailedException;
 import com.swatt.util.general.KeepNewestHash;
 
+//The Base package for representing JSON-RPC 2.0 messages
+import com.thetransactioncompany.jsonrpc2.JSONRPC2Request;
+import com.thetransactioncompany.jsonrpc2.JSONRPC2Response;
+import com.thetransactioncompany.jsonrpc2.client.JSONRPC2Session;
+
 public class MoneroChainNode extends ChainNode {
-    private static final Logger LOGGER = Logger.getLogger(BitcoinChainNode.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(MoneroChainNode.class.getName());
     private static KeepNewestHash transactions;
+    private int requestId = 0;
+    private JSONRPC2Session rpcSession;
+
+    // The JSON-RPC 2.0 server URL
+    URL serverURL = null;
+
+    try
+    {
+        serverURL = new URL("http://jsonrpc.example.com:8080");
+
+    }catch(
+    MalformedURLException e)
+    {
+        // handle exception...
+    }
 
     public static final int POWX_ATOMIC_UNITS = 12;
 
     private JsonRpcHttpClientPool jsonRpcHttpClientPool;
 
-    public MoneroChainNode() {
+    public MoneroChainNode() throws MalformedURLException {
+        // Create new JSON-RPC 2.0 client session
+        JSONRPC2Session mySession = new JSONRPC2Session(serverURL);
     }
 
     @Override
@@ -49,23 +75,39 @@ public class MoneroChainNode extends ChainNode {
             RPCBlock rpcBlock = jsonrpcClient.invoke(XMRMethods.GET_BLOCK, parameters, RPCBlock.class);
 
             BlockData blockData = new BlockData();
-            blockData.setHash(rpcBlock.hash);
-            blockData.setSize(rpcBlock.size);
-            blockData.setHeight(rpcBlock.height);
-            blockData.setVersionHex(rpcBlock.versionHex);
-            blockData.setMerkleRoot(rpcBlock.merkleroot);
-            blockData.setTimestamp(rpcBlock.time);
-            blockData.setNonce(rpcBlock.nonce);
-            blockData.setBits(rpcBlock.bits);
-            blockData.setDifficulty(rpcBlock.difficulty);
-            blockData.setPrevHash(rpcBlock.previousblockhash);
-            blockData.setNextHash(rpcBlock.nextblockhash);
+            blockData.setHash(rpcBlock.blockHeader.hash);
+            blockData.setHeight(rpcBlock.blockHeader.height);
+            blockData.setVersionHex(rpcBlock.versionHex); // TODO address version
+            blockData.setTimestamp(rpcBlock.blockHeader.timestamp);
+            blockData.setNonce(rpcBlock.blockHeader.nonce);
+            blockData.setDifficulty(rpcBlock.blockHeader.difficulty);
+            blockData.setPrevHash(rpcBlock.blockHeader.prevHash);
 
-            // blockData.setReward(BITCOIN_BLOCK_REWARD_BTC);
+            blockData.setReward(rpcBlock.blockHeader.reward);
+
+            /*
+             * The fee will vary based on the base reward and the current
+             * 
+             * block size limit:
+             * 
+             * fee = (R/R0) * (M0/M) * F0
+             * 
+             * R: base reward
+             * 
+             * R0: reference base reward (10 monero)
+             * 
+             * M: block size limit
+             * 
+             * M0: minimum block size limit (60000)
+             * 
+             * F0: 0.002 monero
+             * https://monero.stackexchange.com/questions/4562/how-does-the-dynamic-
+             * blocksize-and-the-dynamic-fees-work-together-in-monero
+             */
 
             blockData.setBlockchainCode(blockchainCode);
 
-            System.out.println("CALCULATING BLOCK: " + rpcBlock.hash);
+            System.out.println("CALCULATING BLOCK: " + rpcBlock.blockHeader.hash);
 
             // calculate(jsonrpcClient, blockData, rpcBlock);
 
@@ -93,4 +135,19 @@ public class MoneroChainNode extends ChainNode {
             throws OperationFailedException {
         return null;
     }
+
+    /*
+     * public BigDecimal getBalance() throws JSONRPC2SessionException { BigDecimal
+     * balance = null; JSONObject result = rpcCall("getbalance", null); if (result
+     * != null) { balance = BigDecimal.valueOf(Long.valueOf("" +
+     * result.get("balance")), 12); } return balance; }
+     * 
+     * private JSONObject rpcCall(String method, List<Object> params) throws
+     * JSONRPC2SessionException { JSONRPC2Request request = new
+     * JSONRPC2Request(method, params, ++requestId); JSONRPC2Response response =
+     * rpcSession.send(request); JSONObject result = null; if
+     * (response.indicatesSuccess()) { result = (JSONObject) response.getResult(); }
+     * else { // TODO: Throw exception
+     * System.err.println(response.getError().getMessage()); } return result; }
+     */
 }
