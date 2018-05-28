ded40a856a55af3c569048c6f2f1f046e29c4892
Unknown <christian@glover-wilson.com>
2018-05-28 17:36:33 -0400

    Move LoggerController

--- src/main/java/com/swatt/blockchain/Main.java
+++ src/main/java/com/swatt/blockchain/Main.java
@@ -13,11 +13,11 @@ import org.slf4j.LoggerFactory;
 import com.swatt.blockchain.ingestor.NodeIngestorManager;
 import com.swatt.blockchain.repository.BlockDataRepository;
 import com.swatt.blockchain.repository.BlockchainNodeInfoRepository;
-import com.swatt.blockchain.service.LoggerController;
 import com.swatt.blockchain.service.NodeManager;
 import com.swatt.blockchain.service.RESTService;
 import com.swatt.blockchain.util.DatabaseUtils;
 import com.swatt.util.general.StringUtilities;
+import com.swatt.util.log.LoggerController;
 import com.swatt.util.sql.ConnectionPool;
 
 public class Main {
@@ -30,7 +30,8 @@ public class Main {
     private static final String API_PORT_PROPERTY = "api.port";
 
     private static final String INGESTOR_ENABLED_PROPERTY = "ingestor.enabled";
-//    private static final String JSON_POOL_SIZE_PROPERTY = "jsonPoolSize"; // TODO move to db
+    // private static final String JSON_POOL_SIZE_PROPERTY = "jsonPoolSize"; // TODO
+    // move to db
     private static final String INGESTOR_OVERWRITE_EXISTING_PROPERTY = "ingestor.overwriteExisting"; // not-implemented yet
 
     // Configurable via environment variables
@@ -41,7 +42,8 @@ public class Main {
     private static final String NODE_OVERRIDE_IP_ENV_VAR_NAME = "NODE_OVERRIDE_IP";
     private static final String NODE_OVERRIDE_PORTS_ENV_VAR_NAME = "NODE_OVERRIDE_PORTS";
 
-    // private static final String INGESTOR_OVERWRITE_EXISTING_ENV_VAR_NAME = "INGESTOR_OVERWRITE_EXISTING"; // not-implemented yet
+    // private static final String INGESTOR_OVERWRITE_EXISTING_ENV_VAR_NAME =
+    // "INGESTOR_OVERWRITE_EXISTING"; // not-implemented yet
 
     private static Properties loadProperties(String filename) throws IOException {
         return mergeProperties(loadPropertiesFromClasspath(filename));
@@ -55,20 +57,20 @@ public class Main {
     private static boolean isEnabled(String environmentValue, boolean defaultValue) {
         if (StringUtilities.isNullOrAllWhiteSpace(environmentValue))
             return defaultValue;
-        
-        if (environmentValue.equalsIgnoreCase("true") || environmentValue.equals("1")) 
+
+        if (environmentValue.equalsIgnoreCase("true") || environmentValue.equals("1"))
             return true;
-        
-        if (environmentValue.equalsIgnoreCase("false") || environmentValue.equals("0")) 
+
+        if (environmentValue.equalsIgnoreCase("false") || environmentValue.equals("0"))
             return false;
-        
+
         return defaultValue;
     }
 
     public static void main(String[] args) {
         try {
             Properties properties = loadProperties(PROPERTIES_FILENAME);
-            
+
             ConnectionPool connectionPool = DatabaseUtils.configureConnectionPoolFromEnvironment(properties);
 
             BlockchainNodeInfoRepository blockchainNodeInfoRepository = new BlockchainNodeInfoRepository(connectionPool);
@@ -87,10 +89,10 @@ public class Main {
                     nodeManager.setOverridePort(keyValue[0], Integer.parseInt(keyValue[1]));
                 }
             }
-            
+
             /** logger **/
             LoggerController.init(properties);
-            
+
             /** api **/
             if (isEnabled(System.getenv(API_ENABLED_ENV_VAR_NAME), getBooleanValue(properties, API_ENABLED_PROPERTY))) {
                 String apiPortValue = getEnvironmentVariableValueOrDefault(API_PORT_ENV_VAR_NAME, API_PORT_PROPERTY, properties);
@@ -99,11 +101,14 @@ public class Main {
                 restService.start();
             }
 
+            // MoneroNode xmrNode = new MoneroNode();
+            // xmrNode.fetchNewBlocks();
+
             /** ingestor **/
             if (isEnabled(System.getenv(INGESTOR_ENABLED_ENV_VAR_NAME), getBooleanValue(properties, INGESTOR_ENABLED_PROPERTY))) {
-            		NodeIngestorManager nodeIngestorManager = new NodeIngestorManager(nodeManager, connectionPool, blockchainNodeInfoRepository, blockDataRepository);
-            		nodeIngestorManager.setOverwriteExisting(getBooleanValue(properties, INGESTOR_OVERWRITE_EXISTING_PROPERTY));
-                        nodeIngestorManager.start();
+                NodeIngestorManager nodeIngestorManager = new NodeIngestorManager(nodeManager, connectionPool, blockchainNodeInfoRepository, blockDataRepository);
+                nodeIngestorManager.setOverwriteExisting(getBooleanValue(properties, INGESTOR_OVERWRITE_EXISTING_PROPERTY));
+                nodeIngestorManager.start();
             }
         } catch (IOException e) {
             LOGGER.error("Exception caught in com.swatt.blockchain.Main: ", e);
