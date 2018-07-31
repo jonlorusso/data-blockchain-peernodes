c1334e4fe509adff698bc24b3005e8596d07d43f
Jon Lorusso <jon@swatt.exchange>
2018-07-30 22:54:13 -0400

    WIP - Add numberOfThreads argument to manual ingestion process

--- src/main/java/com/swatt/blockchain/ingestor/NodeIngestor.java
+++ src/main/java/com/swatt/blockchain/ingestor/NodeIngestor.java
@@ -3,6 +3,8 @@ package com.swatt.blockchain.ingestor;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.time.Instant;
+import java.util.List;
 import java.util.Properties;
 
 import org.slf4j.Logger;
@@ -17,6 +19,7 @@ import com.swatt.blockchain.repository.BlockchainNodeInfoRepository;
 import com.swatt.blockchain.service.NodeManager;
 import com.swatt.blockchain.util.DatabaseUtils;
 import com.swatt.util.general.CollectionsUtilities;
+import com.swatt.util.general.ConcurrencyUtilities;
 import com.swatt.util.general.OperationFailedException;
 import com.swatt.util.log.LoggerController;
 import com.swatt.util.sql.ConnectionPool;
@@ -63,16 +66,20 @@ public class NodeIngestor implements NodeListener {
         }
     }
     
-    private void ingestBlock(long height) throws OperationFailedException, SQLException {
+    public boolean ingestBlock(long height) throws OperationFailedException, SQLException {
         if (existsBlockData(height) && overwriteExisting) {
             BlockData blockData = node.fetchBlockData(height);
             blockDataRepository.replace(blockData);
             LOGGER.info(String.format("Re-ingested block: %d", height));
+            return true;
         } else if (!existsBlockData(height)) {
             BlockData blockData = node.fetchBlockData(height);
             blockDataRepository.insert(blockData);
             LOGGER.info(String.format("Ingested block: %d", height));
+            return true;
         }
+        
+        return false;
     }
 
     public void start() {
@@ -98,41 +105,101 @@ public class NodeIngestor implements NodeListener {
                 }
             }, "HistoricalIngestion-" + node.getCode());
             
-            historicalIngestionThread.start();
+            try {
+                historicalIngestionThread.start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
         node.fetchNewBlocks();
     }
-
-    public static void main(String[] args) throws OperationFailedException, SQLException, IOException {
-        String code = args[0];
-        long start = Long.valueOf(args[1]);
-
+    
+    public static void ingest(String code, int start, int end, int numberOfThreads) throws IOException {
         Properties properties = CollectionsUtilities.loadProperties("config.properties");
-
         LoggerController.init(properties);
-
-        ConnectionPool connectionPool = DatabaseUtils.configureConnectionPoolFromEnvironment(properties);
-        BlockchainNodeInfoRepository blockchainNodeInfoRepository = new BlockchainNodeInfoRepository(connectionPool);
-        BlockDataRepository blockDataRepository = new BlockDataRepository(connectionPool);
-
-        NodeManager nodeManager = new NodeManager(blockchainNodeInfoRepository);
-        Node node = nodeManager.getNode(code);
-
-        long end = start;
-        if (args.length > 2) {
-            end = Long.valueOf(args[2]);
-        } else {
-            end = node.fetchBlockCount();
-        }
-            
-        LOGGER.info("Ingesting " + code + " blocks: " + start + " to " + end);
         
-        NodeIngestor nodeIngestor = new NodeIngestor(node, connectionPool, blockDataRepository);
-        for (long height = start; height < end; height++) {
-            nodeIngestor.ingestBlock(height);
-        }
+        ConnectionPool connectionPool = DatabaseUtils.configureConnectionPoolFromEnvironment(properties);
+        
+        int blocksPerThread = Math.abs(start - end) / numberOfThreads;
+        LOGGER.info("Blocks per thread: " + blocksPerThread);
+                
+        if (start < end) {
+            for (int i = 0; i < numberOfThreads; i++) {
+                final int threadNumber = i;
+                
+                ConcurrencyUtilities.startThread(() -> {
+                    long startms = Instant.now().toEpochMilli();
+                    long blocksIngested = 0;
+                    
+                    LOGGER.info("Start nodeIngestion thread: " + (threadNumber * blocksPerThread) + " -> " + ((threadNumber + 1) * blocksPerThread - 1));
+                    
+                    BlockchainNodeInfoRepository blockchainNodeInfoRepository = new BlockchainNodeInfoRepository(connectionPool);
+                    BlockDataRepository blockDataRepository = new BlockDataRepository(connectionPool);
+                    
+                    NodeManager nodeManager = new NodeManager(blockchainNodeInfoRepository);
+                    Node node = nodeManager.getNode(code);
+    
+                    NodeIngestor nodeIngestor = new NodeIngestor(node, connectionPool, blockDataRepository);
+                    
+                    for (long height = (threadNumber * blocksPerThread); height < ((threadNumber + 1) * blocksPerThread); height++) {
+                        try {
+                            if (nodeIngestor.ingestBlock(height)) {
+                                blocksIngested++;
+                                long now = Instant.now().toEpochMilli();
+                                System.out.println("blocks/second: " + (blocksIngested / ((now - startms) / 1000)));
+                            }
+                        } catch (OperationFailedException | SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        } else {
+            for (int i = numberOfThreads; i > 0; i--) {
+                final int threadNumber = i;
+                
+                ConcurrencyUtilities.startThread(() -> {
+                    long startms = Instant.now().toEpochMilli();
+                    long blocksIngested = 0;
+                    
+                    LOGGER.info("Start nodeIngestion thread: " + (threadNumber * blocksPerThread - 1) + " -> " + ((threadNumber - 1) * blocksPerThread));
+                    
+                    BlockchainNodeInfoRepository blockchainNodeInfoRepository = new BlockchainNodeInfoRepository(connectionPool);
+                    BlockDataRepository blockDataRepository = new BlockDataRepository(connectionPool);
+                    
+                    NodeManager nodeManager = new NodeManager(blockchainNodeInfoRepository);
+                    Node node = nodeManager.getNode(code);
+    
+                    NodeIngestor nodeIngestor = new NodeIngestor(node, connectionPool, blockDataRepository);
 
-        System.exit(0);
+                    for (long height = (threadNumber * blocksPerThread - 1); height > ((threadNumber - 1) * blocksPerThread); height--) {
+                        try {
+                            if (nodeIngestor.ingestBlock(height)) {
+                                blocksIngested++;
+                                long now = Instant.now().toEpochMilli();
+                                System.out.println("blocks/second: " + (blocksIngested / ((now - startms) / 1000)));
+                            }
+                        } catch (OperationFailedException | SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        }
+    }
+    
+    public static void main(String[] args) throws OperationFailedException, SQLException, IOException {
+        try {
+            String code = args[0];
+            int start = Integer.valueOf(args[1]);
+            int end = Integer.valueOf(args[2]);
+            int numberOfThreads = Integer.valueOf(args[3]);
+
+            LOGGER.info("Ingesting " + code + " blocks: " + start + " to " + end + ", " + numberOfThreads + " threads.");
+            ingest(code, start, end, numberOfThreads);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
